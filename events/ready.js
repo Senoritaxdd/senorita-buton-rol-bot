501b9c4ba7baf3a80baae74096c2bfa18bc469b4
Señorita <98999375+Senoritaxdd@users.noreply.github.com>
2022-05-05 16:09:44 +0300

    Add files via upload

---
+++ events/ready.js
@@ -0,0 +1,12 @@
+﻿const Discord = require("discord.js");
+
+module.exports = client => {
+  client.user.setStatus("online");
+  client.user.setPresence({ activity: { name: "Senorita is here?" }, status: "dnd" });
+
+  //Botun Durumu
+  //online=Çevrimiçi
+  //idle=Boşta
+  //dnd=Rahatsız Etmeyin
+  //invisible=Görünmez
+};
